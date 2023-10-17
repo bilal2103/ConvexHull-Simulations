facb467f5fd76b91b822172ae517fe7f814a72b3
Bilal Hassan <97851996+bilal2103@users.noreply.github.com>
2023-10-17 21:40:08 +0500

    Update main.py

--- main.py
+++ main.py
@@ -44,15 +44,42 @@ def Add_Line(p1, p2, c, color):
 class MainMenu:
     def __init__(self):
         self.root = tk.Tk()
+        self.obj = ConvexHull(points)
         self.DisplayMenu()
     def DisplayJarvis(self):
         self.root.destroy()
-        obj.JarvisMarch()
+        self.obj.JarvisMarch()
+    def DisplayGraham(self):
+        self.root.destroy()
+        self.obj.GrahamScan()
+    def DisplayBruteForce(self):
+        self.root.destroy()
+        self.obj.BruteForce()
+    def CloseWindow(self):
+        self.root.destroy()
+        self.obj.root.destroy()
     def DisplayMenu(self):
         self.root.geometry("900x900")
         self.root.title("Main Menu")
+        self.root.protocol("WM_DELETE_WINDOW",self.CloseWindow)
+        self.root.configure(bg="Black")
+        title_lbl = tk.Label(self.root,text = "Simulations for algorithms to obtain Convex Hull", font=('Comic Sans',25))
+
+        lbl1 = tk.Label(self.root,text="Click any of the following buttons:", font=('Comic Sans',20))
+
+        title_lbl.config(bg="Black", fg="White")
+        lbl1.configure(bg="Black",fg="White",anchor='w', justify="left")
         jarvis_btn = tk.Button(self.root,text="Simulate Jarvis March", font=('Comic Sans',18),command=self.DisplayJarvis)
-        jarvis_btn.pack()
+
+        graham_btn = tk.Button(self.root, text="Simulate Graham Scan", font=('Comic Sans', 18),command=self.DisplayGraham)
+
+        bruteforce_btn = tk.Button(self.root, text="Simulate Brute Force approach", font=('Comic Sans', 18),command=self.DisplayBruteForce)
+
+        title_lbl.pack()
+        lbl1.pack(padx=20,pady=70,anchor="w")
+        jarvis_btn.pack(padx=20,pady=10,anchor="w")
+        graham_btn.pack(padx=20,pady=10,anchor="w")
+        bruteforce_btn.pack(padx=20,pady=10,anchor="w")
         self.root.mainloop()
 
 class ConvexHull:
@@ -66,29 +93,30 @@ class ConvexHull:
         self.start = Point(300,500)
         self.screenheight = 700
         self.screenwidth = 700
+        self.root = tk.Tk()
+        self.root.withdraw()
     def display_Hull(self):
         print('Displaying points that are part of the hull')
         for point in self.Hull:
             print(str(point.x) + ',' + str(point.y))
-
+    def CloseWindow(self):
+        self.root.destroy()
+        MainMenu()
     def JarvisMarch(self):
         def proceed():
             result_var.set(False)
-        def CloseWindow():
-            root.destroy()
-            MainMenu()
-        root = tk.Tk()
-        root.geometry(f"{self.screenheight}x{self.screenwidth}")
-        root.protocol("WM_DELETE_WINDOW", CloseWindow)
+
+        self.root.deiconify()
+        self.root.geometry(f"{self.screenheight}x{self.screenwidth}")
+        self.root.protocol("WM_DELETE_WINDOW", self.CloseWindow)
         result_var = tk.BooleanVar()
-        root.configure(bg="Black")
-        root.title('Jarvis March')
-        c = tk.Canvas(root, bg="black", height=700, width=700)
+        self.root.configure(bg="Black")
+        self.root.title('Jarvis March')
+        c = tk.Canvas(self.root, bg="black", height=700, width=700)
         title_label = tk.Label(c,text="Simulation for Jarvis March Algorithm",font=('Comic Sans',15), bg="black", fg="white")
         c.create_window(350,20,window=title_label,anchor="center")
         origin_index = left_most()
         origin = self.Points[origin_index]
-        print(origin)
         for point in self.Points:
             temp = Point(point.x, point.y)
             temp.x = abs(origin.x - temp.x) * 30 + self.start.x
@@ -103,7 +131,7 @@ class ConvexHull:
         l = origin_index
         p = l
         q = 0
-        simulation_speed = 1
+        simulation_speed = 500
         while True:
             self.Hull.append(self.Points[p])
             q = (p + 1) % n
@@ -113,17 +141,16 @@ class ConvexHull:
                 if p == i or q == i or p == i:
                     continue
                 result_var.set(False)
-                root.after(simulation_speed, proceed)
-                root.wait_variable(result_var)
+                self.root.after(simulation_speed, proceed)
+                self.root.wait_variable(result_var)
                 line_id2 = Add_Line(self.updated_points[self.Points[q]], self.updated_points[self.Points[i]], c,
                                     "purple")
                 c.pack()
                 result_var.set(False)
-                root.after(simulation_speed, proceed)
-                root.wait_variable(result_var)
+                self.root.after(simulation_speed, proceed)
+                self.root.wait_variable(result_var)
                 o = orientation(self.Points[p], self.Points[i], self.Points[q])
                 if o == 2:
-                    print('Orientation is counter clockwise')
                     c.delete(line_id)
                     c.delete(line_id2)
                     q = i
@@ -136,14 +163,47 @@ class ConvexHull:
             if p == l:
                 break
         c.pack()
-        root.mainloop()
-
+        self.root.mainloop()
+    '''
+    write your code in the function below aimon, btw deiconify is used to open the screen again which was initially put to sleep
+    in the constructor using root.withdraw()...
+    '''
+    def GrahamScan(self):
+        self.root.deiconify()
+        self.root.geometry(f"{self.screenwidth}x{self.screenheight}")
+        self.root.title("Graham Scan")
+        c = tk.Canvas(self.root,width=700,height=700,bg="Black")
+        title_label = tk.Label(c, text="Simulation for Graham Scan Algorithm", font=('Comic Sans', 15), bg="black",fg="white")
+        c.create_window(350, 20, window=title_label, anchor="center")
+        self.root.protocol("WM_DELETE_WINDOW",self.CloseWindow)
+        c.pack()
+        self.root.mainloop()
+    def BruteForce(self):
+        self.root.deiconify()
+        self.root.geometry(f"{self.screenwidth}x{self.screenheight}")
+        self.root.title("Brute Force")
+        c = tk.Canvas(self.root, width=700, height=700, bg="Black")
+        title_label = tk.Label(c, text="Simulation for Brute force approach", font=('Comic Sans', 15), bg="black",
+                               fg="white")
+        c.create_window(350, 20, window=title_label, anchor="center")
+        self.root.protocol("WM_DELETE_WINDOW", self.CloseWindow)
+        origin = self.Points[left_most()]
+        for point in self.Points:
+            temp = Point(point.x, point.y)
+            temp.x = abs(origin.x - temp.x) * 30 + self.start.x
+            temp.y = self.start.y - abs(origin.x + temp.y) * 30
+            self.updated_points[point] = temp
+            c.create_oval(temp.x, temp.y, temp.x + self.circle_radius, temp.y + self.circle_radius, fill="red")
+            coordinates = tk.Label(c, text=f"{point.x},{point.y}", font=('Comic Sans', 5), bg="black", fg="white")
+            label_window = c.create_window(temp.x, temp.y - 5, window=coordinates)
+        n = len(self.Points)
+        c.pack()
+        self.root.mainloop()
 
 
 
 
 points = []
 Generate_Points()
-obj = ConvexHull(points)
 Main = MainMenu()
 
