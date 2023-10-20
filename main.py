e77894af36e0a04a84fdc9dce48cbc02a856729d
Bilal Hassan <97851996+bilal2103@users.noreply.github.com>
2023-10-20 22:39:01 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import tkinter as tk
 import math as mp
+from functools import cmp_to_key
 from tkinter import messagebox
 import random
-
-
+n = 10
 class Point:
     def __init__(self, x, y):
         self.x = x
@@ -14,15 +14,18 @@ def orientation(p, q, r):
 
     if val == 0:
         return 0
-    elif val > 0:
+    elif val > 0:   #clockwise
         return 1
-    else:
+    else:           #counterclockwise
         return 2
 def CheckLine(lineEndptA, lineEndptB, ptSubject):       #For brute force method, checking which side of the line do points lie on
     return (ptSubject.x - lineEndptA.x) * (lineEndptB.y - lineEndptA.y) - (ptSubject.y - lineEndptA.y) * (
                 lineEndptB.x - lineEndptA.x)
 def distance(p, q):
     return mp.sqrt((p.x - q.x) ** 2 + (p.y - q.y) ** 2)
+def distSq(p1, p2):
+    return ((p1.x - p2.x) * (p1.x - p2.x) +
+            (p1.y - p2.y) * (p1.y - p2.y))
 def left_most() -> int:                 #function for finding leftmost point
     minn = 0
     for i in range(1, len(points)):
@@ -33,7 +36,7 @@ def left_most() -> int:                 #function for finding leftmost point
                 minn = i
     return minn                 #
 def Generate_Points():                      #Function to generate random points
-    while len(points) < 10:
+    while len(points) < n:
         x = random.randint(0, 20)
         y = random.randint(0, 20)
         point = Point(x, y)
@@ -42,6 +45,9 @@ def Generate_Points():                      #Function to generate random points
             points.append(point)
 def Add_Line(p1, p2, c, color):
     return c.create_line(p1.x, p1.y, p2.x, p2.y, fill=color)
+
+
+
 
 class MainMenu:         #for the functionalities of main screen...
     def __init__(self):
@@ -103,7 +109,7 @@ class ConvexHull:
     def __init__(self, p):
         self.Points = p
         self.circle_radius = 10
-        self.start = Point(200,500)
+        self.start = Point(200,550)
         self.screenheight = 700
         self.screenwidth = 700
         self.root = tk.Tk()
@@ -117,7 +123,6 @@ class ConvexHull:
     def CloseWindow(self):
         self.root.destroy()
         MainMenu()
-
     def proceed(self):
         self.result_var.set(True)
     def JarvisMarch(self):
@@ -160,7 +165,6 @@ class ConvexHull:
                 break
         c.pack()
         self.root.mainloop()
-
     def GrahamScan(self):
         c = self.InitializeWindow("Graham Scan","Simulation for Graham scan algorithm")
         n = len(self.Points)
@@ -188,14 +192,150 @@ class ConvexHull:
         c.pack()
         return c
 
-   
+    def graham_scan_utility(self,points):
+
+        if len(points) <= 2 :
+            return points
+
+        def compare(p1, p2):
+            # Find orientation
+            o = orientation(p0, p1, p2)
+            if o == 0:
+                if distSq(p0, p2) >= distSq(p0, p1):
+                    return -1
+                else:
+                    return 1
+            else:
+                if o == 2:
+                    return -1
+                else:
+                    return 1
+        #perform graham scan here
+        n = len(points)
+        ymin = points[0].y
+        min = 0
+        for i in range(1, n):
+            y = points[i].y
+            if ((y < ymin) or
+                    (ymin == y and points[i].x < points[min].x)):
+                ymin = points[i].y
+                min = i
+        points[0], points[min] = points[min], points[0]
+        p0 = points[0]
+        points = sorted(points, key=cmp_to_key(compare))
+        m = 1
+        for i in range(1, n):
+
+            # Keep removing i while angle of i and i+1 is same
+            # with respect to p0
+            while ((i < n - 1) and
+                   (orientation(p0, points[i], points[i + 1]) == 0)):
+                i += 1
+
+            points[m] = points[i]
+            m += 1  # Update size of modified array
+        S = []
+        S.append(points[0])
+        S.append(points[1])
+        S.append(points[2])
+        for i in range(3, m):
+            while ((len(S) > 1) and
+                   (orientation(S[-2], S[-1], points[i]) != 2)):
+                S.pop()
+            S.append(points[i])
+        return S
+
     def Chans(self):
-        k = 2
-        c = self.InitializeWindow("Chans Algorithm","Simulation for Chan's Algorithm")
-
-
+        c = self.InitializeWindow("Chans Algorithm", "Simulation for Chan's Algorithm")
+        Merged_Hull = []
+        for t in range(n):
+            for m in range(1,1<<(1<<t)):
+                Merged_Hull = self.Chans_utility(m,c)
+                if len(Merged_Hull) > 0:
+                    print("Solution found for m = ", m)
+                    t = n-1
+                    break
+        for i in range(1,len(Merged_Hull)):
+            Add_Line(self.updated_points[Merged_Hull[i]],self.updated_points[Merged_Hull[i-1]],c,"white")
+        Add_Line(self.updated_points[Merged_Hull[0]], self.updated_points[Merged_Hull[-1]], c, "white")
         c.pack()
         self.root.mainloop()
+    def Chans_utility(self,k,c):
+        print("Utility function called for k: ", k)
+        subsets = []
+        subsets_size = n // k
+        for i in range(0, n, subsets_size):
+            subsets.append(self.Points[i:i + subsets_size])
+        hulls = [self.graham_scan_utility(subset) for subset in subsets]
+        colors = ["red","blue","green","yellow","orange","purple","pink","brown","cyan","magenta","lime","olive","gold","silver","gray","lightblue","lightgreen","lightgray"]
+        temp = 0
+        lines = []
+        for h in hulls:
+            for i in range(1, len(h)):
+                lines.append(Add_Line(self.updated_points[h[i - 1]], self.updated_points[h[i]], c, colors[temp]))
+            lines.append(Add_Line(self.updated_points[h[0]], self.updated_points[h[-1]], c, colors[temp]))
+            temp += 1
+        c.pack()
+        def extreme_hullpoint():
+            h, p = 0, 0
+            for i in range(len(hulls)):
+                min_index, min_y = 0, hulls[i][0].y
+                for j in range(1, len(hulls[i])):
+                    if hulls[i][j].y < min_y:
+                        min_y = hulls[i][j].y
+                        min_index = j
+                if hulls[i][min_index].y < hulls[h][p].y:
+                    h = i
+                    p = min_index
+            return (h, p)
+
+        def tangent(v,p):
+            n = len(v)
+            l=0
+            r,l_before,l_after = n,orientation(p,v[0],v[n-1]),orientation(p,v[0],v[(l+1)%n])
+            while l < r:
+                c = ((l + r) >> 1)
+                c_before = orientation(p, v[c], v[(c - 1) % n])
+                c_after = orientation(p, v[c], v[(c + 1) % n])
+                c_side = orientation(p, v[l], v[c])
+                if c_before != 1 and c_after != 1:
+                    return c;
+                elif (c_side == 2) and (l_after == 1 or l_before == l_after) or (c_side == 1 and c_before == 1):
+                    r = c
+                else:
+                    l = c
+                l_before = -c_after
+                l_after = orientation(p, v[l], v[(l + 1) % n])
+            return l
+        def next_hullpoint():
+
+            lpoint = merged_hull[-1]
+            p = hulls[lpoint[0]][lpoint[1]]
+            next = (lpoint[0],(lpoint[1]+1) % len(hulls[lpoint[0]]))
+            for h in range(len(hulls)):
+                if h == lpoint[0]:
+                    continue
+                s = tangent(hulls[h],p)
+                q = hulls[next[0]][next[1]]
+                r = hulls[h][s]
+                t = orientation(p,q,r)
+                if t == 1 or (t == 0 and distance(p,r) > distance(p,q)):
+                    next = (h,s)
+            return next
+
+        merged_hull_points = []
+        merged_hull = []
+        merged_hull.append(extreme_hullpoint())
+        for i in range(k):
+            p = next_hullpoint()
+            if p == merged_hull[0]:
+                for j in range(len(merged_hull)):
+                    merged_hull_points.append(hulls[merged_hull[j][0]][merged_hull[j][1]])
+                return merged_hull_points
+            merged_hull.append(p)
+        for i in range(len(lines)):
+            c.delete(lines[i])
+        return merged_hull_points
     def BruteForce(self):
         n = len(self.Points)
         c = self.InitializeWindow("Brute Force","Simulation for brute force approach")
